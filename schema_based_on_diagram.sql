0bf13351c718cd2ee0da9832189de633468f0841
gabicilfone <cilfonegabriel@gmail.com>
2023-03-06 17:39:23 -0300

    add FK INDEXES

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,3 +14,13 @@ alter table invoice_items add foreign key(treatment_id) references treatments(id
 
 create table if not exists invoices(id int generated always as identity, total_amount decimal, generated_at timestamp, payed_at timestamp, 
 medical_history_id int references medical_histories(id), primary key(id));
+
+create index patient_id on medical_histories(patient_id asc);
+
+create index medical_treatment_id on medical_history_treatment(treatment_id asc);
+create index medical_histories_id on medical_history_treatment(medical_history_id asc);
+
+create index invoice_id on invoice_items(invoice_id asc);
+create index treatment_id on invoice_items(treatment_id asc);
+
+create index medical_history_id on invoices(medical_history_id asc);
